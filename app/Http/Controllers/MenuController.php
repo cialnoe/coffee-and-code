9a083760ccb208c53d03b4f91d108b091ce83d13
febrianamaniani25-rgb <febrianamaniani25@gmail.com>
2026-06-23 23:54:44 +0700

    menus

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -1,5 +1,16 @@
-public function index()
-{
+<?php
 
-    return view('menus.index');
+namespace App\Http\Controllers; 
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller; // Tambahkan ini jika belum ada
+
+class MenuController extends Controller // <-- PASTIKAN HURUF 'M' DAN 'C' KAPITAL!
+{
+    public function index()
+    {
+        return view('menus.index');
+    }
+    
+    // ... method CRUD lainnya (create, store, edit, dll)
 }
