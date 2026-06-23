834ca3b213ff43b3afdf0d19eda2c9d86e72bab4
febrianamaniani25-rgb <febrianamaniani25@gmail.com>
2026-06-23 23:45:21 +0700

    menus

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,5 @@
+public function index()
+{
+
+    return view('menus.index');
+}
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MemberController; // Pindahkan ke atas agar rapi
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\ReservationController;
 
+// Halaman utama otomatis diarahkan ke daftar members
 Route::get('/', function () {
-    // Kita arahkan langsung ke halaman index members
     return redirect()->route('members.index');
 });
 
-Route::resource('members', MemberController::class);
-Route::get('/', function () {
-    return view('welcome');
-});
 
-use App\Http\Controllers\ReservationController;
+// Route Resource untuk Fitur Kelompok
+Route::resource('members', MemberController::class); 
+Route::resource('menus', MenuController::class);
 
-// Route untuk Modul Reservasi Coworking Space
+// Route untuk Modul Reservasi Coworking Space (Kerjaan Teman)
 Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
 Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
 Route::patch('/reservations/{id}/status/{status}', [ReservationController::class, 'updateStatus'])->name('reservations.updateStatus');
